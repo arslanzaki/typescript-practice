08f59c8f67003cac5cb3cb594b69abaf4995aa62
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-09-30 23:35:05 +0500

    Initial Commit

---
+++ intro.js
@@ -0,0 +1,4 @@
+console.log("HELLO");
+console.log(Math.round(4.656568711));
+console.log("ARSLAN");
+var pi = 3.145665;
---
+++ intro.ts
@@ -0,0 +1,7 @@
+console.log("HELLO");
+
+console.log(Math.round(4.656568711));
+
+console.log("ARSLAN")
+
+let pi: number = 3.145665;
