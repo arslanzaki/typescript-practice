45255a430a5d329b95d9fd4c0fd60f05eb9e7246
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-10-02 12:44:05 +0500

    Functions

--- Functions/functions.ts
+++ Functions/functions.ts
@@ -30,10 +30,56 @@ userData("Arslan", 22, false);
 
 // ######################################################
 
-function greet(name: string = 'Mr.X') {
+function greet(name: string = "Mr.X") {
   return `Hello, ${name}`;
 }
 
-greet('Arslan');
+greet("Arslan");
 greet();
 // greet(123); ERROR
+
+// ######################################################
+// FUNCTION RETURN TYPES
+
+const addNums = (x: number, y: number): number => {
+  // x + y ERROR
+  return x + y;
+};
+
+// ######################################################
+// ANONYMOUS FUNCTIONS
+
+const numbers = [1, 2, 3];
+numbers.forEach((num) => {
+  // Type Of 'num' will be NUMBER
+});
+
+const colors = ["red", "green", "blue"];
+colors.map((color) => {
+  // Type of 'color' will be STRING
+});
+
+// ######################################################
+// VOID TYPE
+
+function doSomething(name: string): void {
+  // By Default, the function's return type will be VOID
+  // We can also specify that
+  console.log(name);
+
+  // return name; ERROR
+}
+
+// ######################################################
+// NEVER TYPE
+
+function makeError(msg: string): never {
+  throw new Error(msg)
+}
+
+function gameLoop(): never {
+  while(true) {
+    console.log("Loop")
+  }
+}
+
